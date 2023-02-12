7637a2288b5a913c3b6cdd5ac4ef9498dcfd2c8d
blanche <79345528+cd-blanche@users.noreply.github.com>
2023-02-11 22:56:27 -0500

    Add validation to user input to prevent inputs over two decimals

--- js/main.js
+++ js/main.js
@@ -2,27 +2,37 @@
 
 
 
-
-
 function calcIncomeTotal() {
+
   const currentIncome = document.getElementById('current-income');
-  const userInput = document.getElementById('user-input');
-  const userInputIncome = parseFloat(userInput.value);
+  const incomeUserInput = document.getElementById('income-user-input');
+  const userInputIncome = parseFloat(incomeUserInput.value);
   const errorText = document.getElementById('income-error-text');
 
   if (userInputIncome > 0) {
-    errorText.style.display = "none";
+    // resets error visibility
+    errorText.style.visibility = "hidden";
     const newIncome = userInputIncome + parseFloat(currentIncome.innerHTML);
-    currentIncome.innerHTML = String(newIncome);
+    currentIncome.innerHTML = (Math.round(newIncome * 100) / 100).toFixed(2);
+    // clears user input
+    incomeUserInput.value = "";
   } else {
     // print out error text
-    errorText.style.display = "block";
-    userInput.placeholder = "Please enter a valid number.";    
-    // remove user input
-    userInput.value = "";
+    errorText.style.visibility = "visible";
+    incomeUserInput.placeholder = "Try again";    
+    // clears user input
+    incomeUserInput.value = "";
   }
-
-
-  // to do
-  // always end in .xx cents
 }
+
+// Prevent user from entering more than 2 decimal place values
+
+function validate(e) {
+  // stores the initial value in a separate constant t
+  const t = e.value;
+  // checks whether or not there is "." located within the input
+  // if true, it will grab all the characters until "." and then the first 3 characters from "." including "."
+  // if false - aka no "." is found, then it will return the original input t
+  e.value = (t.indexOf(".") >= 0) ? (t.substr(0, t.indexOf(".")) + t.substr(t.indexOf("."), 3)) : t;
+}
+
