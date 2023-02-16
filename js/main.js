85ff2c1a2818ddfbd717e6103aae6894097d8400
blanche <79345528+cd-blanche@users.noreply.github.com>
2023-02-15 20:58:25 -0500

    Add basic dark mode/light mode functionality
    To reduce strain on the eyes when working on this project especially at night

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function calcTotal(current, userInput) {
     currentInput.value = "";
     currentInput.placeholder = "Please enter a numerical value"
   }
-}
+};
 
 // Prevent user from entering more than 2 decimal place values
 function validate(e) {
@@ -26,5 +26,28 @@ function validate(e) {
   // if true, it will grab all the characters until "." and then the first 3 characters from "." including "."
   // if false - aka no "." is found, then it will return the original input t
   e.value = (t.indexOf(".") >= 0) ? (t.substr(0, t.indexOf(".")) + t.substr(t.indexOf("."), 3)) : t;
-}
+};
 
+// To do - Add custom light mode/dark mode
+
+const themeMode = document.querySelector('#theme-mode');
+const root = document.querySelector(':root');
+
+
+themeMode.addEventListener('click', (e) => {
+  const theme = e.target.getAttribute('data-theme');
+  
+  if (theme == 'light') {
+    themeMode.dataset.theme = 'dark';
+    themeMode.innerHTML = 'Switch to<br>Light Mode';
+    root.style.setProperty('--main-radial-gradient', 'linear-gradient(to bottom, #414345, #232526)');
+    root.style.setProperty('--h1-font-color', 'white');
+  } else {
+    themeMode.dataset.theme = 'light';
+    themeMode.innerHTML = 'Switch to<br>Dark Mode';
+    root.style.setProperty('--main-radial-gradient', 'linear-gradient(120deg, #fdfbfb 0%, #ebedee 100%)');
+    root.style.setProperty('--h1-font-color', 'black');
+  }
+})
+
+// To do - Add number validation - checks whether input is a numerical value or not
